3f3d811f3be450075a4a94f10a419ae225c95d1f
dxhInter <dxh0530@gmail.com>
2024-01-15 21:33:22 -0500

    first commit

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import datetime
 
 from flask import Flask, jsonify, request
-from flask_cors import CORS
 import time
 import json
 import os
@@ -9,12 +8,12 @@ import os
 app = Flask(__name__)
 
 '''
-this is a function that to show a json string from data.json file
+this is a function that to show a json string from dht11_data.json file
 '''
-@app.route('/show_data')
+@app.route('/show_data', methods=['GET'])
 def show_data():
-    if os.path.exists('data.json'):
-        with open('data.json', 'r') as json_file:
+    if os.path.exists('dht11_data.json'):
+        with open('dht11_data.json', 'r') as json_file:
             data = json.load(json_file)
         return jsonify(data), 200
     return jsonify('No data available'), 400
@@ -23,17 +22,21 @@ def show_data():
 this is a function that to receive a json string from edge computer
 in json string format
 it's like the following:
-{"temperatureAndHumidity": "25.7+53.0", "timestamp": "2024-01-15 16:45:10"}
+{"temperature": 25.4, "humidity": 55.0, "timestamp": "2024-01-15 17:10:18"}
 '''
 @app.route('/resv_dht_data', methods=['POST'])
 def resv_dht_data():
     try:
-        temp_and_humi = request.args['temperatureAndHumidity']
+        # temp_and_humi = request.args['temperatureAndHumidity']
+        data = request.json
+        temperature = data['temperature']
+        humidity = data['humidity']
         formatted_datetime = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-        with open('data.json', 'w') as outfile:
+        with open('dht11_data.json', 'w') as outfile:
             json.dump(
                 {
-                    'temperatureAndHumidity': temp_and_humi,
+                    'temperature': temperature,
+                    'humidity': humidity,
                     'timestamp': formatted_datetime
                 },
                 outfile
@@ -46,9 +49,6 @@ def resv_dht_data():
         return jsonify({'error': 'Missing temperature and humidity parameter'}), 400
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-
-
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
--- sensorSend.py
+++ sensorSend.py
@@ -1,30 +1,34 @@
 from socket import socket, gethostbyname, AF_INET, SOCK_DGRAM
 import requests
-# URL of the cloud server where the data will be pushed to
 
-# IP Addresses and Port numbers of the UDP servers running 
-# on two RRi's which have the LEDs connected.
-RPI_1_IP = '10.20.0.20'
+PRI_IP_1 = '10.20.0.20'
 CLOUD_SERVER_URL = 'http://127.0.0.1:5001'
 
-
-# Setting up a simple UDP server, used to receive data from
-# the RPi having the sensor connected.
 PORT_NUMBER = 8090
 SIZE = 1024
 hostName = gethostbyname('0.0.0.0')
 mySocket = socket(AF_INET, SOCK_DGRAM)
 mySocket.bind((hostName, PORT_NUMBER))
-print("Edge computer listening for input on port {}".format(PORT_NUMBER))
+# print("Edge computer listening for input on port {}".format(PORT_NUMBER))
 
-# Infinite loop to continuously look for data being sent
 while True:
-    # Receiving data from the Sensor RPi.
-    # (data,addr) = mySocket.recvfrom(SIZE)
-    # print('Received data: "{}". This will be sent to the server.'.data)
-    # print('Received data: "{}". This will be sent to the server.'.format(data))
+    # Receiving data from the Sensor DHT11.
     (data, addr) = mySocket.recvfrom(SIZE)
+    # Decoding the byte[] from the socket
     received_data = data.decode()
     # Print the received data
     print(f'Received data: "{received_data}". This will be sent to the cloud server.')
-    requests.post('{}/resv_dht_data'.format(CLOUD_SERVER_URL), params={'temperatureAndHumidity': received_data})
+    try:
+        temperature, humidity = received_data.split('+')
+        temperature = float(temperature)
+        humidity = float(humidity)
+    except ValueError:
+        print("Received data is not in the expected format.")
+        continue
+        # create a dictionary to send to cloud server
+    data_to_send = {
+        'temperature': temperature,
+        'humidity': humidity
+    }
+    # send a request to cloud server
+    requests.post('{}/resv_dht_data'.format(CLOUD_SERVER_URL), json=data_to_send)
