610057540558bfe3491450be3d65bfbb1eb816e6
dxhInter <dxh0530@gmail.com>
2024-01-16 22:58:02 -0500

    second commit

--- sensorSend.py
+++ sensorSend.py
@@ -2,14 +2,25 @@ from socket import socket, gethostbyname, AF_INET, SOCK_DGRAM
 import requests
 
 PRI_IP_1 = '10.20.0.20'
-CLOUD_SERVER_URL = 'http://127.0.0.1:5001'
+PRI_IP_2 = '10.20.0.22'
+CLOUD_SERVER_URL = 'http://10.20.0.9:5001'
 
 PORT_NUMBER = 8090
 SIZE = 1024
 hostName = gethostbyname('0.0.0.0')
 mySocket = socket(AF_INET, SOCK_DGRAM)
 mySocket.bind((hostName, PORT_NUMBER))
+
+
 # print("Edge computer listening for input on port {}".format(PORT_NUMBER))
+
+def judge_code(code):
+    # send ok or fail to led
+    if code == 200:
+        mySocket.sendto("ok".encode(), PRI_IP_1)
+    else:
+        mySocket.sendto("fail".encode(), PRI_IP_2)
+
 
 while True:
     # Receiving data from the Sensor DHT11.
@@ -32,3 +43,5 @@ while True:
     }
     # send a request to cloud server
     requests.post('{}/resv_dht_data'.format(CLOUD_SERVER_URL), json=data_to_send)
+    code = requests.get('{}/show_data'.format(CLOUD_SERVER_URL))
+    judge_code(code)
